d001e0b7835ca1f521e79b367b4de86917bed9d7
hpc9909 <hpc9909@gmail.com>
2025-10-19 13:59:21 +0900

    sts init 251019

---
+++ src/main/java/com/example/jys251019/Jys251019Application.java
@@ -0,0 +1,13 @@
+package com.example.jys251019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jys251019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jys251019Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jys251019/Jys251019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jys251019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jys251019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
