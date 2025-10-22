e31da035f83436be92268495c25f9117c3a52627
hpc9909 <hpc9909@gmail.com>
2025-10-22 12:45:51 +0900

    book part2_3장 complete ~110p

---
+++ src/main/java/com/example/jys251019/controller/ArticleController.java
@@ -0,0 +1,20 @@
+package com.example.jys251019.controller;
+import com.example.jys251019.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
+    }
+
+}
--- src/main/java/com/example/jys251019/controller/JysController.java
+++ src/main/java/com/example/jys251019/controller/JysController.java
@@ -1,8 +1,10 @@
 package com.example.jys251019.controller;
 
+import com.example.jys251019.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JysController {
@@ -18,4 +20,6 @@ public class JysController {
         model.addAttribute("nickname","곰살쟁이");
         return "goodbye";
     }
+
+
 }
